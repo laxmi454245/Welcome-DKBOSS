c1f520be1c6745defa68cba72d210e11777f16ad
laxmi454245 <laxmi454245@gmail.com>
2026-06-24 10:35:22 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+
+    alertMessage:
+    "🚨 Welcome DK BOSS Alert Karna Chata Hu Koi Payment Nhi Karega 🚨",
+
+    tickerSpeed:15,
+
+    telegramUsername:"pujawachhori",
+
+    tokenLength:40,
+
+    tokenExpireTime:60
+
+};
