96b5202c7aa6b7673724ee8d2538d6c9580d9621
laxmi454245 <laxmi454245@gmail.com>
2026-06-24 10:55:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
 
-    alertMessage:
-    "🚨 Welcome DK BOSS Alert Karna Chata Hu Koi Payment Nhi Karega 🚨",
+alertMessage:
+"🚨 ALERT NOTICE 🚨 DKB Jal wala Telegram ID kisi aur vyakti ne login kar liya hai. Kripya us ID par kisi bhi prakar ka payment na karein. ✅ Payment sirf official bot mein hi karein. Saath hi, hamare paas server aur domain ka poora data aa chuka hai, isliye kisi bhi fake ya unauthorized ID se satark rahen. Dhanyavaad.",
 
-    tickerSpeed: 15,
+tickerSpeed: 15,
 
-    telegramUsername: "DKB0SBOT",
+telegramUsername: "DKB0SBOT",
 
-    tokenLength: 40,
+tokenLength: 40,
 
-    tokenExpireTime: 60
+tokenExpireTime: 60
 
 };
